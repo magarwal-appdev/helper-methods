1af5b58092ba4f1c9ee99228c4fbd0c418f312f3
Megha-Avee <megha@neonforlife.com>
2022-04-10 21:45:24 +0000

    rewrote routes using helper methods

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -19,16 +19,20 @@ class MoviesController < ApplicationController
         render "movies/index"
       end
     end
+
+    # render "movies/index"
   end
 
   def show
-    the_id = params.fetch(:id)
+    # the_id = params.fetch(:id)
 
-    matching_movies = Movie.where({ :id => the_id })
+    # matching_movies = Movie.where({ :id => the_id })
 
-    @the_movie = matching_movies.first
+    # @the_movie = matching_movies.first
 
-    render({ :template => "movies/show.html.erb" })
+    @the_movie = Movie.find(params.fetch(:id))
+
+    render "movies/show"
   end
 
   def create
@@ -51,7 +55,7 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render({ :template => "movies/edit.html.erb" })
+    render "movies/edit"
   end
 
   def update
@@ -63,9 +67,9 @@ class MoviesController < ApplicationController
 
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+      redirect_to movie_path(the_movie.id), notice: "Movie updated successfully." 
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => "Movie failed to update successfully." })
+      redirect_to movie_path(the_movie.id), alert: "Movie failed to update successfully."
     end
   end
 
@@ -75,6 +79,6 @@ class MoviesController < ApplicationController
 
     the_movie.destroy
 
-    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+    redirect_to movies_path, notice: "Movie deleted successfully."
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,16 @@ Rails.application.routes.draw do
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create"
-  get "/movies/new" => "movies#new"
+  post "/movies" => "movies#create", as: :movies #gives us movies_url and movies_path, it just returns string and totally ignores the verb, so we don't have to rename other routes with different verb
+  get "/movies/new" => "movies#new", as: :new_movie #new_movie_url and new_movie_path
           
   # READ
   get "/movies" => "movies#index"
-  get "/movies/:id" => "movies#show"
+  get "/movies/:id" => "movies#show", as: :movie #movie_path() and expects an argument to populate id segment
   
   # UPDATE
   patch "/movies/:id" => "movies#update"
-  get "/movies/:id/edit" => "movies#edit"
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie #edit_movie_path(), expects an argument to populate :id segment
   
   # DELETE
   delete "/movies/:id" => "movies#destroy"
